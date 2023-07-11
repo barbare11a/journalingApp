b9669af8397a6caf79f6e7c34fd29cf7d032d77b
Barbarella <85380852+barbare11a@users.noreply.github.com>
2023-07-10 20:07:48 -0400

    initial document

---
+++ JournalApp/JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by barbarella castillo on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
