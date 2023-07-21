50cf343015b81e577fe17ceca3a44d64b0227626
johnny <johnny.casares.7@gmail.com>
2023-07-21 18:37:16 -0400

    johnny pushing johan into GitHub

--- JournalApp/JournalApp/JournalAppApp.swift
+++ JournalApp/JournalApp/JournalAppApp.swift
@@ -2,7 +2,7 @@
 //  JournalAppApp.swift
 //  JournalApp
 //
-//  Created by barbarella castillo on 7/10/23.
+//  Created by barbarella castillo on 7/10/23./Users/administrator/Downloads/CalendarExampleTutorial-main-2/CalendarExampleTutorial/AppDelegate.swift
 //
 
 import SwiftUI
