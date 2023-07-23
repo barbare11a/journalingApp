3d64cb03bdfd92edc283457018396e38475ead23
Maria Paula Macedo <104603278+miapaulita@users.noreply.github.com>
2023-07-22 20:04:28 -0400

    Update FeelingsPicker.swift

--- JournalApp/JournalApp/FeelingsPicker.swift
+++ JournalApp/JournalApp/FeelingsPicker.swift
@@ -7,24 +7,165 @@
 //feelings page
 
 import SwiftUI
+import AVFoundation
 
 struct FeelingsPicker: View {
-
+    
+    @State  var session: AVAudioSession?
+    @State  var recorder: AVAudioRecorder?
+    @State  var myString = ""
+    
+    
     var body: some View {
-        
-        VStack {
-            HStack{
+        NavigationView{
+           
+            VStack(alignment: .center, spacing: 50, content:{
+            
+                Text("How are you feeling today?")
+                .font(.title)
+                .fontWeight(.bold)
+
+                HStack{
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "happy")){
+                            VStack{
+                                Image("happy")
+                                Text("happy")
+                                    .foregroundColor(.black)
+                            }
+                        }
+
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "calm")){
+                            VStack{
+                                Image("calm")
+                                Text("calm")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "sad")){
+                            VStack{
+                                Image("sad")
+                                Text("sad")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                }
                 
-                Image("happy")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Text("this should be the feelings page")
+                HStack{
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "angry")){
+                            VStack{
+                                Image("angry")
+                                Text("angry")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "anxious")){
+                            VStack{
+                                Image("anxious")
+                                Text("anxious")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "loved")){
+                            VStack{
+                                Image("loved")
+                                Text("loved")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                }
+                
+                HStack{
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "embarrassed")){
+                            VStack{
+                                Image("embarrassed")
+                                Text("embarrassed")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "bored")){
+                            VStack{
+                                Image("bored")
+                                Text("bored")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                    NavigationLink(
+                        destination: RecordFeelings(session: $session, recorder: $recorder, myEmotion: "jealous")){
+                            VStack{
+                                Image("jealous")
+                                Text("jealous")
+                                    .foregroundColor(.black)
+
+                            }
+                        }
+                    Spacer()
+                }
+                
+                NavigationLink(
+                    destination: FeelingsSummaryView(),
+                    label: {
+                        Text("skip to journal")
+                                    })
                 
             }
+        )
             
-        }
-        .padding()
+            .navigationBarItems(
+                leading:
+                    NavigationLink(
+                    destination: ProfilePageView(),
+                    label: {
+                        Image(systemName: "person.circle")
+                            .foregroundColor(.black)
+                                    })
+                    .padding(.leading, 20),
+                trailing:
+                NavigationLink(
+                    destination: Text("CalenderView goes here"),
+                    label: {
+                        Image(systemName: "calendar")
+                        .padding(.trailing, 20)
+                        .foregroundColor(.black)
+                                    })
+                )
+            
+    
+        }//end of NavigationView
+        
+    }//end of some View
+    
+}//end of View
+
+
+struct EmotionRecorder: View{
+    var body: some View{
+        Text("Take a moment to record your feelings.")
     }
 }
 
@@ -33,5 +174,3 @@ struct FeelingsPicker_Previews: PreviewProvider {
         FeelingsPicker()
     }
 }
-
-
